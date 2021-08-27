4c07656c1793ae893ce79ce5098216de28bc03f6
Sk Kaushik <skkaushik021@gmail.com>
2021-08-27 13:15:41 +0530

    base url

--- jwt-auth/src/index.js
+++ jwt-auth/src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { HashRouter } from "react-router-dom";
-
 import "./styles/index.css";
 
 ReactDOM.render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
